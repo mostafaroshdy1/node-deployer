cc5ce78ab666fc3ee3a2f4e3d691a7492996e462
Belal Abo Ata <belalbelalmohamed1234@gmail.com>
2024-06-22 19:16:41 +0300

    feat: add the redirect url end point

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,40 +5,46 @@ import { AuthService } from 'src/services/auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
 
-  @Get(':provider')
-  @UseGuards(DynamicAuthGuard)
-  async auth(@Req() req: Request) {
-    console.log(req.user);
-  }
+	@Get(':provider')
+	@UseGuards(DynamicAuthGuard)
+	async auth(@Req() req: Request) {
+		console.log(req.user);
+	}
 
-  @Get(':provider/callback')
-  @UseGuards(DynamicAuthGuard)
-  async authRedirect(@Req() req: Request, @Res() res: Response) {
-    const accessToken = req.user['accessToken'];
-    res
-      .status(302)
-      .redirect(
-        `${process.env.FRONT_END_URL}/auth/callback?access_token=${accessToken}&refresh_token=${accessToken}&provider=${req.params.provider}`,
-      );
-  }
+	@Get(':provider/callback')
+	@UseGuards(DynamicAuthGuard)
+	async authRedirect(@Req() req: Request, @Res() res: Response) {
+		const accessToken = req.user['accessToken'];
+		res
+			.status(302)
+			.redirect(
+				`${process.env.FRONT_END_URL}/auth/callback?access_token=${accessToken}&refresh_token=${accessToken}&provider=${req.params.provider}`,
+			);
+	}
 
-  // @Get(':provider/callback/repo')
-  // async gitlabAuthCallback(@Req() req: Request, @Res() res: Response) {
-  //   const accessToken = req.headers.authorization.split(' ')[1];
+	@Get('redirectUrl/:provider')
+	async redirectUrl(@Req() req: Request, @Res() res: Response) {
+		const url = await this.authService.getRedirectUrl(req.params.provider);
+		return res.json({ url });
+	}
 
-  //   try {
-  //     const user = await this.authService.getGitLabUser(accessToken);
-  //     const repos = await this.authService.getGitLabRepos(accessToken);
-  //     const response = {
-  //       user: user,
-  //       repos: repos,
-  //     };
-  //     return res.json(response);
-  //   } catch (error) {
-  //     console.log(error);
-  //     return res.status(500).json({ message: 'Internal Server Error' });
-  //   }
-  // }
+	// @Get(':provider/callback/repo')
+	// async gitlabAuthCallback(@Req() req: Request, @Res() res: Response) {
+	//   const accessToken = req.headers.authorization.split(' ')[1];
+
+	//   try {
+	//     const user = await this.authService.getGitLabUser(accessToken);
+	//     const repos = await this.authService.getGitLabRepos(accessToken);
+	//     const response = {
+	//       user: user,
+	//       repos: repos,
+	//     };
+	//     return res.json(response);
+	//   } catch (error) {
+	//     console.log(error);
+	//     return res.status(500).json({ message: 'Internal Server Error' });
+	//   }
+	// }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -9,88 +9,111 @@ import axios from 'axios';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly userService: UserService,
-    private readonly jwtService: JwtService,
-  ) {}
+	constructor(
+		private readonly userService: UserService,
+		private readonly jwtService: JwtService,
+	) {}
 
-  async validateUser(
-    profile: Profile,
-    accessToken: string,
-  ): Promise<User | null> {
-    const { id, username, displayName, emails, provider } = profile;
-    const email: string = emails[0].value;
-    const foundUser = await this.userService.findByEmailAndProvider(
-      email,
-      provider,
-    );
+	async validateUser(profile: Profile, accessToken: string): Promise<User | null> {
+		const { id, username, displayName, emails, provider } = profile;
+		const email: string = emails[0].value;
+		const foundUser = await this.userService.findByEmailAndProvider(email, provider);
 
-    if (foundUser) {
-      foundUser.accessToken = accessToken;
-      return await this.userService.update(foundUser.id, { accessToken });
-    }
+		if (foundUser) {
+			foundUser.accessToken = accessToken;
+			return await this.userService.update(foundUser.id, { accessToken });
+		}
 
-    const user: CreateUserDto = {
-      provider: provider,
-      providerId: id,
-      username: username,
-      name: displayName,
-      email: emails[0].value,
-      accessToken: accessToken,
-    };
-    return this.userService.create(user);
-  }
+		const user: CreateUserDto = {
+			provider: provider,
+			providerId: id,
+			username: username,
+			name: displayName,
+			email: emails[0].value,
+			accessToken: accessToken,
+		};
+		return this.userService.create(user);
+	}
 
-  async createJwtTokens(
-    user: UserEntity,
-    scope: string,
-  ): Promise<{ access_token: string; refresh_token: string }> {
-    const payload = {
-      id: user.id,
-      email: user.email,
-      scope: scope,
-    };
-    const [access_token, refresh_token] = await Promise.all([
-      this.jwtService.signAsync(payload, {
-        expiresIn: process.env.JWT_EXPIRATION,
-      }),
-      this.jwtService.signAsync(payload, {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRATION,
-      }),
-    ]);
-    return {
-      access_token,
-      refresh_token,
-    };
-  }
+	async createJwtTokens(
+		user: UserEntity,
+		scope: string,
+	): Promise<{ access_token: string; refresh_token: string }> {
+		const payload = {
+			id: user.id,
+			email: user.email,
+			scope: scope,
+		};
+		const [access_token, refresh_token] = await Promise.all([
+			this.jwtService.signAsync(payload, {
+				expiresIn: process.env.JWT_EXPIRATION,
+			}),
+			this.jwtService.signAsync(payload, {
+				expiresIn: process.env.REFRESH_TOKEN_EXPIRATION,
+			}),
+		]);
+		return {
+			access_token,
+			refresh_token,
+		};
+	}
 
-  async getGitLabRepos(accessToken: string) {
-    try {
-      const response = await axios.get('https://gitlab.com/api/v4/projects', {
-        params: { owned: true },
-        headers: { Authorization: `Bearer ${accessToken}` },
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching GitLab repositories:', error.response.data);
-      throw new InternalServerErrorException(
-        'Failed to fetch GitLab repositories: ' +
-          error.response.data.error_description,
-      );
-    }
-  }
+	async getGitLabRepos(accessToken: string) {
+		try {
+			const response = await axios.get('https://gitlab.com/api/v4/projects', {
+				params: { owned: true },
+				headers: { Authorization: `Bearer ${accessToken}` },
+			});
+			return response.data;
+		} catch (error) {
+			console.error('Error fetching GitLab repositories:', error.response.data);
+			throw new InternalServerErrorException(
+				'Failed to fetch GitLab repositories: ' + error.response.data.error_description,
+			);
+		}
+	}
 
-  async getGitLabUser(accessToken: string) {
-    try {
-      const response = await axios.get('https://gitlab.com/api/v4/user', {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      });
-      return response.data;
-    } catch (error) {
-      console.error('Error fetching GitLab user:', error.response.data);
-      throw new InternalServerErrorException(
-        'Failed to fetch GitLab user: ' + error.response.data.error_description,
-      );
-    }
-  }
+	async getGitLabUser(accessToken: string) {
+		try {
+			const response = await axios.get('https://gitlab.com/api/v4/user', {
+				headers: { Authorization: `Bearer ${accessToken}` },
+			});
+			return response.data;
+		} catch (error) {
+			console.error('Error fetching GitLab user:', error.response.data);
+			throw new InternalServerErrorException(
+				'Failed to fetch GitLab user: ' + error.response.data.error_description,
+			);
+		}
+	}
+
+	async getRedirectUrl(provider: string) {
+		let rootUrl: string;
+		let options;
+		switch (provider) {
+			case 'github':
+				rootUrl = 'https://github.com/login/oauth/authorize';
+				options = {
+					client_id: process.env.GITHUB_CLIENT_ID,
+					redirect_uri: process.env.GITHUB_CALLBACK_URL,
+					scope: 'scope',
+				};
+				break;
+			case 'gitlab':
+				rootUrl = 'https://gitlab.com/oauth/authorize';
+				options = {
+					client_id: process.env.GITLAB_CLIENT_ID,
+					redirect_uri: process.env.GITLAB_CALLBACK_URL,
+					scope: 'api read_api read_user sudo',
+					response_type: 'code',
+				};
+				break;
+		}
+
+		const queryString = new URLSearchParams(options).toString();
+
+		const url = `${rootUrl}?${queryString.toString()}`;
+
+		return url;
+	}
 }
